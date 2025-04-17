46c5fc3a7c3ae8eef983e7d268e7d89a03c574f2
Patrick Serrano <patrickcmserrano@gmail.com>
2025-04-17 17:01:27 -0300

    Adiciona novos posts sobre fundamentos de Java, incluindo exceções, manipulação de dados e programação orientada a objetos. Atualiza a estrutura do blog e adiciona conteúdo à página inicial e sobre.

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -14,6 +14,30 @@
   
 	const posts: Post[] = [
 	  {
+		id: 'java-pt3',
+		title: 'Construindo Aplicações Java Robustas: Exceções, Arquivos e Threads',
+		excerpt: 'Descubra como lidar com exceções, manipular arquivos e implementar threads em Java.',
+		date: '2024-03-17',
+		readTime: '15 min',
+		tags: ['Java', 'Exceções', 'Threads']
+	  },
+	  {
+		id: 'java-pt2',
+		title: 'Gerenciando Dados em Java: Strings, Collections e JDBC',
+		excerpt: 'Explore como manipular dados em Java com Strings, Collections e conectividade com bancos de dados.',
+		date: '2024-03-16',
+		readTime: '12 min',
+		tags: ['Java', 'Dados', 'Collections']
+	  },
+	  {
+		id: 'java-intro',
+		title: 'Começando com Java: Sintaxe, Fluxo e POO para Construir uma Base Sólida',
+		excerpt: 'Aprenda os fundamentos de Java, incluindo sintaxe, controle de fluxo e programação orientada a objetos.',
+		date: '2024-03-15',
+		readTime: '10 min',
+		tags: ['Java', 'Programação', 'POO']
+	  },
+	  {
 		id: 'arvores',
 		title: 'Entendendo Árvores e Árvores Binárias: Um Guia para Engenheiros de Software',
 		excerpt: 'Explorando o mundo das árvores e árvores binárias, estruturas de dados essenciais para qualquer engenheiro de software.',
@@ -61,30 +85,6 @@
 		date: '2024-03-08',
 		readTime: '10 min',
 		tags: ['Requisitos', 'Engenharia', 'Software'],
-	  },
-	  {
-		id: 'java-intro',
-		title: 'Começando com Java: Sintaxe, Fluxo e POO para Construir uma Base Sólida',
-		excerpt: 'Aprenda os fundamentos de Java, incluindo sintaxe, controle de fluxo e programação orientada a objetos.',
-		date: '2024-03-15',
-		readTime: '10 min',
-		tags: ['Java', 'Programação', 'POO']
-	  },
-	  {
-		id: 'java-pt2',
-		title: 'Gerenciando Dados em Java: Strings, Collections e JDBC',
-		excerpt: 'Explore como manipular dados em Java com Strings, Collections e conectividade com bancos de dados.',
-		date: '2024-03-16',
-		readTime: '12 min',
-		tags: ['Java', 'Dados', 'Collections']
-	  },
-	  {
-		id: 'java-pt3',
-		title: 'Construindo Aplicações Java Robustas: Exceções, Arquivos e Threads',
-		excerpt: 'Descubra como lidar com exceções, manipular arquivos e implementar threads em Java.',
-		date: '2024-03-17',
-		readTime: '15 min',
-		tags: ['Java', 'Exceções', 'Threads']
 	  }
 	];
   </script>
